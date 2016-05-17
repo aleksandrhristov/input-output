a39e9ce608537d848dcfa2c4c0b47bafa7f921a3
Aleksandar Hristov <alexanderhristov93@abv.bg>
2016-05-17 17:38:18 +0300

    fixed file names

--- src/transferobject/Demo.java
+++ src/transferobject/Demo.java
@@ -6,8 +6,8 @@ public class Demo {
 
     public static void main(String[] args) throws IOException {
         TransferObject transferObject = new TransferObject();
-        String myFile = "files.txt";
-        String myFile1 = "ffiles.txt";
+        String myFile = "input.txt";
+        String myFile1 = "output.txt";
         InputStream in = new FileInputStream(myFile);
         OutputStream out = new FileOutputStream(myFile1);
         transferObject.transfer(in, out, 0, 2);
