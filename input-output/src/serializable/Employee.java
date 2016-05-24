4217ca131d31f77f843c8257b8714cb56733d038
Aleksandar Hristov <alexanderhristov93@abv.bg>
2016-05-24 14:50:17 +0300

    ready with the serializable task

---
+++ input-output/src/serializable/Employee.java
@@ -0,0 +1,39 @@
+package serializable;
+
+/**
+ * Created by clouway on 24.05.16.
+ */
+public class Employee implements java.io.Serializable {
+
+    private String name;
+    private String address;
+    private  long EGN;
+    private int number;
+
+    public Employee(String name, String address, long EGN, int number) {
+        this.name = name;
+        this.address = address;
+        this.EGN = EGN;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public long getEGN() {
+        return EGN;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void mailCheck() {
+        System.out.println("Mailing a check to " + name + " " + address);     
+    }
+}
